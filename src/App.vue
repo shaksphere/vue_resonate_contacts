85c9bb03305c625beff178122f68d65036282507
shaksphere <110363828+shaksphere@users.noreply.github.com>
2023-03-29 01:17:35 +1100

    updated responsive app for Resonate

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,200 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <img src="./assets/contacts.svg">
+    <h1>Contacts</h1>
+    <div class="search">
+      <input type="text" v-model="searchQuery" placeholder="Search contacts..." class="search-input">
+    </div>
+    <ul>
+      <li v-for="contact in filteredContacts" :key="contact.id" class="contact">
+        <div class="contact__header" @click="contact.showMenu = !contact.showMenu">
+          <div class="contact__name">{{ contact.id }}. {{ contact.name }}</div>
+          <div class="contact__menu-toggle">{{ contact.showMenu ? 'Hide -' : 'Show Details +' }}</div>
+        </div>
+        <div class="contact__menu" v-if="contact.showMenu">
+          <div class="contact__item">
+            <span class="contact__item-label">Username:</span>
+            <span class="contact__item-value">{{ contact.username }}</span>
+          </div>
+          <div class="contact__item">
+            <span class="contact__item-label">Email:</span>
+            <span class="contact__item-value">{{ contact.email }}</span>
+          </div>
+          <div class="contact__item">
+            <span class="contact__item-label">Address:</span>
+            <span class="contact__item-value">{{ contact.address.street }}, {{ contact.address.suite }}, {{ contact.address.city }}, {{ contact.address.zipcode }}</span>
+          </div>
+          <div class="contact__item">
+            <span class="contact__item-label">Phone:</span>
+            <span class="contact__item-value">{{ contact.phone }}</span>
+          </div>
+          <div class="contact__item">
+            <span class="contact__item-label">Website:</span>
+            <span class="contact__item-value">{{ contact.website }}</span>
+          </div>
+          <div class="contact__item">
+            <span class="contact__item-label">Company:</span>
+            <span class="contact__item-value">{{ contact.company.name }}</span>
+          </div>
+        </div>
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import './styles.scss';
+
+
+export default {
+  
+  name: 'App',
+  data() {
+    return {
+      contacts: [],
+      searchQuery: ''
+    }
+  },
+  computed: {
+    filteredContacts() {
+      // Filter contacts based on search query
+      return this.contacts.filter(contact => {
+        return contact.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      })
+    }
+  },
+  created() {
+   
+    // Fetch contacts from API using fetch
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        // Add "showMenu" property to each contact
+        this.contacts = data.map(contact => {
+          return {
+            ...contact,
+            showMenu: false
+          }
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+</script>
+
+<style lang='scss' scoped>
+@import "variables";
+.search {
+  margin-bottom: 20px;
+
+  input[type="text"] {
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    max-width: 800px;
+
+    &:focus {
+      outline: none;
+    }
+  }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.search-input {
+  padding: 8px 12px;
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  font-size: 1.2rem;
+  width: 100%;
+  margin-bottom: 20px;
 }
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+.contact {
+  background-color: $primary-color;
+  color: $font-color;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 20px;
+  margin-bottom: 20px;
+  
+}
+
+.contact__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+}
+
+.contact__name {
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+  margin-right: 50px; /* add margin to the right */
+}
+
+.contact__menu-toggle {
+  font-weight: bold;
+  font-size: 1.2rem;
+
+}
+
+.contact__menu {
+  margin-top: 10px;
+
+}
+
+
+
+.contact__item:hover {
+  color: $secondary-color;
+
+}
+.contact__item {
+  margin-bottom: 5px;
+  text-align: start;
+}
+
+.contact__item-label {
+  font-weight: bold;
+  margin-right: 5px;
+  flex-basis: 30%;
+
+}
+
+.contact__item-info-value {
+  flex-basis: 70%;
+}
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+}
+
+@media only screen and (max-width: 767px) {
+  .contact__item-info {
+    display: block;
+  }
+
+  .contact__item-info-item {
+    flex-basis: auto;
+
+  }
+
+  .contact__item-info-label {
+    display: block;
+    margin-bottom: 5px;
+  }
+}
+
+
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sass from 'sass'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default {
+  plugins: [
+    vue(),
+  ],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        implementation: sass,
+      },
+    },
+  },
+}
